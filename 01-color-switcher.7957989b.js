f13e11ebdf4653707d8bb98f840f6ed2dc1ea19f
EllenBredis <106671470+EllenBredis@users.noreply.github.com>
2022-11-15 09:37:03 +0000

    Deploying to gh-pages from @ EllenBredis/goit-js-hw-09@54f584922c96ea1ed8970da60726546879f90c78 🚀

---
+++ 01-color-switcher.7957989b.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=(document.querySelector(".color"),document.querySelector("[data-start]")),d=document.querySelector("[data-stop]");d.disabled=!0,t.addEventListener("click",(()=>{timerId=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0,d.disabled=!1})),d.addEventListener("click",(()=>{clearInterval(timerId),t.removeAttribute("disabled","disabled"),d.setAttribute("disabled","disabled")}));
+//# sourceMappingURL=01-color-switcher.7957989b.js.map
